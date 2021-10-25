e82cd72697e83fc226817cf8e062e8148ba50745
DataEvans <prodigyevans@gmail.com>
2021-10-25 15:16:35 +0100

    final push

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-"# ecommerce_with_spring" 
-"# ecommerce_with_spring" 
+#e-shop ecommerce project
+E-shop is an e-commerce project built with the 
+following spring framework; java, mySQL and maven as a build 
+tool
+
+#Features
+RestAPI that includes the following. Add product to
+the database. Find products by id. find all products. Delete product
+
+Spring MVC with thymeleaf template engine for viewing
+product details.
+
+#Installation
+step 1: clone repository
+step 2: install dependencies; cd into the root folder
+and run the following command
+
+'mvn install'
+
+step 3: Setup database: run sql script
+'src/main/resource/db/setup-db.sql'
+
+step 4: Run the project
+'mvn spring-boot-run'
--- src/main/java/com/ecommerce/shop/web/controller/HomeController.java
+++ src/main/java/com/ecommerce/shop/web/controller/HomeController.java
@@ -1,16 +1,50 @@
 package com.ecommerce.shop.web.controller;
 
 
+import com.ecommerce.shop.Service.ProductService;
+import com.ecommerce.shop.data.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/say-hello")
-    public @ResponseBody String getWelcomePage(){
-        return "Welcome to my first Spring application";
 
+    @Autowired
+    ProductService productServiceImpl;
+
+    @GetMapping(value = {"/", "/index"})
+    public String getIndex(Model model){
+        List<Product> productList = productServiceImpl.findAll();
+        model.addAttribute("products", productList);
+        return "index";
+    }
+
+
+    @GetMapping("/about")
+    public @ResponseBody String getAbout(){
+        return "about";
+    }
+
+    @GetMapping("/product")
+    public @ResponseBody String getProduct(){
+        return "product";
+    }
+
+
+    @GetMapping("/services")
+    public @ResponseBody String getServices(){
+        return "services";
+
+    }
+
+    @GetMapping("/singles")
+    public @ResponseBody String getSingles(){
+        return "singles";
     }
 }
